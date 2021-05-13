f71d9806fc8bb899ea2487cf3ac2a568370a9eb0
oss5824 <45011625+oss5824@users.noreply.github.com>
2021-05-13 23:36:33 +0900

    Firebase finish

--- project9/app/src/main/java/com/example/aop_part2_chpater09/MainActivity.kt
+++ project9/app/src/main/java/com/example/aop_part2_chpater09/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aop_part2_chpater09
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -8,11 +9,11 @@ import com.google.firebase.messaging.FirebaseMessaging
 class MainActivity : AppCompatActivity() {
 
 
-    private val resultTextView: TextView by lazy{
+    private val resultTextView: TextView by lazy {
         findViewById<TextView>(R.id.resultTextView)
     }
 
-    private val firebaseToken:TextView by lazy{
+    private val firebaseToken: TextView by lazy {
         findViewById<TextView>(R.id.firebaseTokenTextView)
     }
 
@@ -22,13 +23,30 @@ class MainActivity : AppCompatActivity() {
 
         initFirebase()
     }
-    private fun initFirebase(){
+
+    private fun initFirebase() {
         FirebaseMessaging.getInstance().token
-            .addOnCompleteListener{task->
-                if(task.isSuccessful){
-                    firebaseToken.text=task.result
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseToken.text = task.result
                 }
 
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        setIntent(intent)//TODO 이미 STACK에 쌓여있는 intent를 호출했을 때 갱신하는 용도이고
+        //TODO intent를 교체해주는 작업 필요
+        updateResult(true)
+    }
+
+    private fun updateResult(isNewIntent: Boolean = false) {
+        resultTextView.text = (intent.getStringExtra("notificationType") ?: "앱 런쳐")+if (isNewIntent) {
+            "(으)로 갱신했습니다."
+        } else {
+            "(으)로 실행했습니다."
+        }
     }
 }
--- project9/app/src/main/java/com/example/aop_part2_chpater09/MyFirebaseMessagingService.kt
+++ project9/app/src/main/java/com/example/aop_part2_chpater09/MyFirebaseMessagingService.kt
@@ -1,9 +1,20 @@
 package com.example.aop_part2_chpater09
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     //TODO 들어올 때마다 토큰이 바뀌어버리기 때문에 그것을 방지하기위해
     //TODO 바뀔 때마다 토큰을 서버로 보내줌
@@ -11,7 +22,91 @@ class MyFirebaseMessagingService: FirebaseMessagingService() {
         super.onNewToken(p0)
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        createNotificationChannel()
+
+        val type = remoteMessage.data["type"]?.let { NotificationType.valueOf(it) }
+
+        type ?: return
+
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["message"]
+
+
+
+        NotificationManagerCompat.from(this)
+            .notify(type.id, createNotification(type, title, message))
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = CHANNEL_DESCRIPTION
+
+            (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+                .createNotificationChannel(channel)
+        }
+    }
+
+    private fun createNotification(
+        type: NotificationType,
+        title: String?,
+        message: String?
+    ): Notification {
+        val intent = Intent(this,MainActivity::class.java).apply{
+            putExtra("notificationType","${type.title}타입")
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            //TODO 이미 A라는 화면이 있는데 또 다시 A로 넘어가면 이상하니까
+            //TODO STACK으로 화면을 또 쌓는게 아니라 기존의 ACTIVITY를 갱신하는 방식으로 구현하는 것
+        }
+
+        val pendingIntent= PendingIntent.getActivity(this,type.id,intent,FLAG_UPDATE_CURRENT)
+        //TODO PENDINGINTENT는 내가 실행을 하는 것이 아니라 권한을 넘겨 주는 것
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        when (type) {
+            NotificationType.NORMAL -> Unit
+            NotificationType.EXPANDABLE -> {
+                notificationBuilder.setStyle(
+                    NotificationCompat.BigTextStyle()
+                        .bigText(
+                            "test"
+                        )
+                )
+            }
+            NotificationType.CUSTOM -> {
+                notificationBuilder
+                    .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+                    .setCustomContentView(
+                        RemoteViews(
+                            packageName,
+                            R.layout.view_custom_notification
+                        ).apply{
+                            setTextViewText(R.id.title,title)
+                            setTextViewText(R.id.message,message)
+                        }
+                    )
+            }
+
+        }
+        return notificationBuilder.build()
+    }
+
+    companion object {
+        private const val CHANNEL_NAME = "Emoji Party"
+        private const val CHANNEL_DESCRIPTION = "Emoji Party를 위한 채널"
+        private const val CHANNEL_ID = "Channel Id"
     }
 }
